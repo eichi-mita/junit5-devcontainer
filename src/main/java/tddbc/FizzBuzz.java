cbe0bee987bd170ab61926c19e3e1fa71f560667
eichisanden <eichisanden@users.noreply.github.com>
2023-07-01 06:40:13 +0000

    fizz

--- src/main/java/tddbc/FizzBuzz.java
+++ src/main/java/tddbc/FizzBuzz.java
@@ -2,6 +2,9 @@ package tddbc;
 
 public class FizzBuzz {
     public String convert(int num) {
+        if (num % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(num);
     }
 }
--- src/test/java/tddbc/FizzBuzzTest.java
+++ src/test/java/tddbc/FizzBuzzTest.java
@@ -2,24 +2,29 @@ package tddbc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzbuzz;
+
+    @BeforeEach
+    void 前準備() {
+        fizzbuzz = new FizzBuzz();
+    }
+
     @Test
     void _1を渡すと文字列1を返す() throws Exception {
-        // 準備 arange or given
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        // 実行 act or when
-        // 検証 assert or then
         assertEquals("1", fizzbuzz.convert(1));
     }
 
     @Test
     void _2を渡すと文字列2を返す() throws Exception {
-        // 準備 arange or given
-        FizzBuzz fizzbuzz = new FizzBuzz();
-        // 実行 act or when
-        // 検証 assert or then
         assertEquals("2", fizzbuzz.convert(2));
+    }
+
+    @Test
+    void _3を渡すと文字列Fizzを返す() throws Exception {
+        assertEquals("Fizz", fizzbuzz.convert(3));
     }
 }
